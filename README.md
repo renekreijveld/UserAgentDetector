5285b5c714baa152f4608ca40b775253f002f688
Rene Kreijveld <email@renekreijveld.nl>
2013-04-20 08:55:40 +0200

    Inital setup

---
+++ README.md
@@ -0,0 +1,14 @@
+User Agent Detector
+====================================
+
+Functions
+---------
+
+* This system plugin detects the user agent of yur website visitor and sets a session variable accordingly
+
+* Based on the user agent, the plugin detects if the site is running on a desktop pc, tablet or smartphone.
+  It can also detect if the visitor is a spider bot (search engine).
+
+* Session variable that is set: ualayout.
+  Possible values: desktop, tablet, mobile, bot.
+  
---
+++ uadetector.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * ---------------------------------------------------------------------------------------------------------
+ * 	User Agent detector plugin
+ *
+ * Copyright (C) 2013 Rene Kreijveld. All rights reserved.
+ *
+ * User Agent detector is free software and is distributed under the GNU General Public License,
+ * and as distributed it may include or be derivative of works licensed under the GNU
+ * General Public License or other free or open source software licenses.
+ * ---------------------------------------------------------------------------------------------------------
+ **/
+
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+jimport( 'joomla.plugin.plugin' );
+
+/**
+ * UADetector Plugin
+ */
+class plgSystemUadetector extends JPlugin
+{
+	public function onAfterInitialise()
+	{
+		include_once(dirname(__FILE__).'/lib/Mobile_Detect.php');
+
+		$detect = new Mobile_Detect();
+		$layout = ($detect->isMobile() ? ($detect->isTablet() ? 'tablet' : 'mobile') : 'desktop');
+		if (($detect->is('Bot')) || ($detect->is('MobileBot'))) $layout = 'bot';
+
+		// store user agent layout in session variable.
+		$session = JFactory::getSession();
+		$session->set('ualayout', $layout);
+	}
+}
